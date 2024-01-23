59a27cec0f5812be30bbe93c3f573f719c822465
Eduardo Neves <62307330+snowedz@users.noreply.github.com>
2024-01-23 08:27:44 -0300

    Cleaning up the code

--- profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import my_profile_view
-
-app_name = 'profiles'
-
-urlpatterns = [
-    path('myprofile/',my_profile_view, name='my-profile-view')
-]
--- profiles/utils.py
+++
@@ -1,4 +0,0 @@
-import uuid
-
-def get_random_id():
-    id = str(uuid.uuid4().hex[:8])
